e8fae8a474718d40536258232d01e89dd48f25b1
humphrey <humphrey.codrington@zoopla.co.uk>
2021-10-11 14:24:38 +0100

    added server side validation to my quiz routes by adding express validatior

--- quiz-manager/routes/quizzes.js
+++ quiz-manager/routes/quizzes.js
@@ -1,26 +1,37 @@
-var express = require("express");
-var router = express.Router();
+const express = require("express");
+const router = express.Router();
+const { body, validationResult } = require("express-validator");
 
-var quizService = require("../services/quizService");
+const quizService = require("../services/quizService");
 
-router.post("/", function (req, res) {
+//create quiz post
+router.post("/", body("name").isLength({ min: 8 }), function (req, res) {
+
+  //server side validation
+  const errors = validationResult(req);
+  if (!errors.isEmpty()) {
+    return res.status(400).json({ errors: errors.array() });
+  }
+  
   function onSuccess(result) {
     res.redirect("/quizzes");
   }
   quizService.createQuiz(req.body, onSuccess);
 });
 
+//render create new page
 router.get("/new", function (req, res, next) {
   res.render("quizzes/new");
 });
 
+//get all quizzes
 router.get("/", function (req, res, next) {
   function onSuccess(quizzes) {
     if (!quizzes) {
       res.render("error", { message: "no quizzes exits" });
     }
     res.render("quizzes", { quizzes: quizzes, message: "Quizzes page" });
-  } 
+  }
   quizService.getAllQuizzes(onSuccess);
 });
 
